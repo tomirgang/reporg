8c64e3ac0f9e4d3a5fb86387c218fc77ec0657e9
Tom Irgang <me@tomirgang.de>
2023-12-22 23:16:47 +0100

    Rollenspezifische Menüeinträge

--- frontend/src/components/side_menu.rs
+++ frontend/src/components/side_menu.rs
@@ -2,11 +2,13 @@ use yew::prelude::*;
 use yew_router::prelude::*;
 
 use crate::app::MembersRoute;
+use crate::app::{AppState, Role};
 use crate::components::user::User;
 
 struct SideNavItem {
     link: MembersRoute,
     label: String,
+    role: Role,
 }
 
 #[function_component(Menu)]
@@ -15,34 +17,49 @@ pub fn menu() -> Html {
         vec![
             SideNavItem {
                 link: MembersRoute::One,
-                label: "One".to_owned(),
+                label: "Guest".to_owned(),
+                role: Role::Guest,
             },
             SideNavItem {
                 link: MembersRoute::Two,
-                label: "Two".to_owned(),
+                label: "Organizer".to_owned(),
+                role: Role::Organizer,
             },
             SideNavItem {
                 link: MembersRoute::Three,
-                label: "Three".to_owned(),
+                label: "Supporter".to_owned(),
+                role: Role::Supporter,
             },
         ]
     });
 
     let route = use_route::<MembersRoute>().unwrap();
 
+    let roles = match use_context::<AppState>() {
+        Some(state) => match state.user {
+            Some(user) => user.roles,
+            None => vec![],
+        },
+        None => vec![],
+    };
+
     html! {
         <div id="side_menu" class="side_menu">
             <User />
             {
                 nav_items.iter().map(|nav_item| {
-                    let is_active = nav_item.link == route;
-                    html! {
-                        <Link<MembersRoute>
-                            to={nav_item.link.clone()}
-                            classes={classes!(if is_active {"active"} else { "" })}
-                        >
-                            {nav_item.label.clone()}
-                        </Link<MembersRoute>>
+                    if roles.contains(&nav_item.role) {
+                        let is_active = nav_item.link == route;
+                        html! {
+                            <Link<MembersRoute>
+                                to={nav_item.link.clone()}
+                                classes={classes!(if is_active {"active"} else { "" })}
+                            >
+                                {nav_item.label.clone()}
+                            </Link<MembersRoute>>
+                        }
+                    } else {
+                        html! {}
                     }
                 }).collect::<Html>()
             }
